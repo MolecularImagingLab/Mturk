b9865d8c37b5835f65f713c3f5aa761719e7a794
ltuominen <tuominenlj@gmail.com>
2020-08-11 16:46:47 -0400

    First Commit

---
+++ DFAQ.js
@@ -0,0 +1,17 @@
+options_trial_2 = ['over a year ago','9 – 12 months ago','6 – 9 months ago','3 – 6 months ago','1 – 3 months ago',
+   'less than 1 month ago', 'last week', 'this week', 'yesterday', 'today', 'I am currently high'];
+
+options_trial_2B = ['I am not at all high', 'I am a little bit high','I am moderately high','I am very high','I am extremely high'];
+
+options_trial_3 = ["I do not use cannabis",  "less than once a year", "once a year", "once every 3-6 months (2-4 times/yr)",
+   "once every 2 months (6 times/yr)", "once a month (12 times/yr)", "2 – 3 times a month", "once a week", "twice a week", "3 – 4 times a week",
+   "5 – 6 times a week", "once a day",  "more than once a day"];
+
+options_trial_4 =["less than 1 month", "1 – 3 months",  "3 – 6 months",  "6 – 9 months",  "9 – 12 months",  "1 – 2 years",
+  "2 – 3 years", "3 – 5 years",  "5 – 10 years",  "10 – 15 years",  "15 – 20 years",  "more than 20 years"]
+
+options_trial_5 = [ "I did not use cannabis", "less than once a year", "once a year", "once every 3-6 months (2-4 times/yr)",
+  "once every 2 months (6 times/yr.)", "once a month", "2 – 3 times a month", "once a week", "twice a week", "3 – 4 times a week",
+  "5 – 6 times a week", "once a day", "more than once a day"]
+
+options_trial_6 = ["0 days",  "1 day",  "2 days",  "3 days",  "4 days",  "5 days",  "6 days",  "7 days",]
---
+++ MAP-SR.js
@@ -0,0 +1,74 @@
+
+// Intstructions are all the same
+var MAPinstruction = 'This scale consists of a number of questions about motivation and pleasure. Indicate to what extent these questions apply to you.';
+
+// motivation
+var motivationItems = [
+  "In the past week how motivated have you been to be around other people and do things with them?",
+  "In the past week how much effort have you made to actually do things with other people?",
+  "In the past week how motivated have you been to go to work or school or look for a job or class to take?",
+  "In the past week how much effort have you made to do things at work or school? (If you are not working or going to school, how much effort have you made to look for a job or go to school.)",
+  "In the past week how motivated have you been to do hobbies or other recreational activities?",
+  "In the past week how much effort have you made to actually do any hobbies or recreational activities?"
+];
+
+var motivationobj = {
+  scale1: ["not at all motivated",
+  "",
+  "",
+  "",
+  "very motivated"],
+  scale2: ["no effort",
+  "",
+  "",
+  "",
+  "very much effort"]
+};
+
+var motivationReverse = Array(motivationItems.length).fill(false);
+
+// social pleasure
+var socialItems = [
+  "In the past week, what is the most pleasure you experienced from being with other people?",
+  "In the past week, how often have you experienced pleasure from being with other people?",
+  "Looking ahead to being with other people in the next few weeks, how much pleasure do you expect you will experience from being with others?"
+];
+
+// recreational pleasure
+var recreationalItems = [
+  "In the past week, what is the most pleasure you experienced from hobbies, recreation, or from work?",
+  "In the past week, how often have you experienced pleasure from hobbies, recreation, or from work?",
+  "Looking ahead to the next few weeks, how much pleasure do you expect you will experience from your hobbies, recreation, or work?"
+];
+
+// pleasureReverese and pleasureScale apply to both
+var pleasureReverse =  Array(socialItems.length).fill(false);
+
+var pleasureobj = {
+  scale1: ["no pleasure",
+  "",
+  "",
+  "",
+  "extreme pleasure"],
+  scale2: ["not at all",
+  "",
+  "",
+  "",
+  "very often"]
+};
+
+// Relationships
+var relationshipItems = [
+  "When it comes to close relationships with your family members, how important have these relationships been to you over the past week?",
+  "When it comes to having a close relationship with a romantic partner, how important has this type of relationship been to you over the past week?",
+  "When it comes to close relationships with your friends, how important have these relationships been to you over the past week?"
+];
+
+var relationshipScale = ["not at all important to me",
+  "",
+  "",
+  "",
+  "extremely important to me"
+];
+
+var relationshipReverse =  Array(relationshipItems.length).fill(false);;
