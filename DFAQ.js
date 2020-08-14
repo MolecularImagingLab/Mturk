a4816f2060f821c0e305fb3e2dd4ee848939e5e0
ltuominen <tuominenlj@gmail.com>
2020-08-14 16:26:03 -0400

    Created DFAQ_generic and DFAQ_obj which appear more attractive options

--- DFAQ.js
+++ DFAQ.js
@@ -15,3 +15,18 @@ options_trial_5 = [ "I did not use cannabis", "less than once a year", "once a y
   "5 – 6 times a week", "once a day", "more than once a day"]
 
 options_trial_6 = ["0 days",  "1 day",  "2 days",  "3 days",  "4 days",  "5 days",  "6 days",  "7 days",]
+
+options_trial_8 = ["1 – 5 times in my life", "6 – 10 times in my life", "11 – 50 times in my life",
+"51 –100 times in my life", "101 – 500 times in my life", "501 – 1000 times in my life",
+"1001 – 2000 times in my life", "2001 – 5000 times in my life", "5001 – 10,000 times in my life",
+"More than 10,000 times in my life"]
+
+options_trial_9 =[ "I do not use cannabis at all",
+"I only use cannabis on weekends",
+"I only use cannabis on weekdays",
+"I use cannabis on weekends and weekdays"]
+
+options_trial_10 = [
+  "I do not use cannabis at all", "12 – 18 hours after waking up", "9 – 12 hours after waking up", "6 – 9 hours after waking up",
+  "3 – 6 hours after waking up", "1 – 3 hours after waking up",  "within 1 hour of waking up", "within ½ hour of waking up", "immediately upon waking up"
+]
---
+++ DFAQ_obj.js
@@ -0,0 +1,35 @@
+Items = {
+  generic_questions: [
+    "Which of the following best captures when you last used cannabis?",
+    "How high are you right now?",
+    "Which of the following best captures the average frequency you currently use cannabis?",
+    "Which of the following best captures how long you have been using cannabis at this frequency?",
+    "Before the period of time you indicated above, how frequently did you use cannabis",
+    "How many days of the past week did you use cannabis?",
+    "Which of the following best captures the number of times you have used cannabis in your entire life?",
+    "Which of the following best captures your pattern of cannabis use throughout the week?",
+    "How many hours after waking up do you typically first use cannabis?"
+  ],
+
+  generic_options_0: ["over a year ago", "9 – 12 months ago","6 – 9 months ago","3 – 6 months ago","1 – 3 months ago",
+     "less than 1 month ago", "last week", "this week", "yesterday", "today", "I am currently high"],
+  generic_options_1: ["I am not at all high", "I am a little bit high","I am moderately high","I am very high", "I am extremely high"],
+  generic_options_2: ["I do not use cannabis",  "less than once a year", "once a year", "once every 3-6 months (2-4 times/yr)",
+     "once every 2 months (6 times/yr)", "once a month (12 times/yr)", "2 – 3 times a month", "once a week", "twice a week", "3 – 4 times a week",
+     "5 – 6 times a week", "once a day",  "more than once a day"],
+  generic_options_3: ["less than 1 month", "1 – 3 months",  "3 – 6 months",  "6 – 9 months",  "9 – 12 months",  "1 – 2 years",
+    "2 – 3 years", "3 – 5 years",  "5 – 10 years",  "10 – 15 years",  "15 – 20 years",  "more than 20 years"],
+  generic_options_4: [ "I did not use cannabis", "less than once a year", "once a year", "once every 3-6 months (2-4 times/yr)",
+    "once every 2 months (6 times/yr)", "once a month", "2 – 3 times a month", "once a week", "twice a week", "3 – 4 times a week",
+    "5 – 6 times a week", "once a day", "more than once a day"],
+  generic_options_5: ["0 days",  "1 day",  "2 days",  "3 days",  "4 days",  "5 days",  "6 days",  "7 days"],
+  generic_options_6: ["1 – 5 times in my life", "6 – 10 times in my life", "11 – 50 times in my life",
+  "51 –100 times in my life", "101 – 500 times in my life", "501 – 1000 times in my life",
+  "1001 – 2000 times in my life", "2001 – 5000 times in my life", "5001 – 10,000 times in my life",
+  "More than 10,000 times in my life"],
+  generic_options_7: [ "I do not use cannabis at all", "I only use cannabis on weekends","I only use cannabis on weekdays",
+  "I use cannabis on weekends and weekdays"],
+  generic_options_8: [ "I do not use cannabis at all", "12 – 18 hours after waking up", "9 – 12 hours after waking up", "6 – 9 hours after waking up",
+    "3 – 6 hours after waking up", "1 – 3 hours after waking up",  "within 1 hour of waking up", "within ½ hour of waking up",
+    "immediately upon waking up"]
+}
